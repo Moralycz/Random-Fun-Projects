fe8e25c156ffdc0c003695f0dd2940570cc8c9ae
Moralycz <144354307+Moralycz@users.noreply.github.com>
2023-10-21 12:39:02 +0800

    Update fibonacciAndGoldenRatio.py

--- fibonacciAndGoldenRatio.py
+++ fibonacciAndGoldenRatio.py
@@ -3,7 +3,7 @@ from statistics import mean
 def main():
     firstSequence = int(input("First Sequence: "))
     secondSequence = int(input("Second Sequence: "))
-    sequences = int(input("Sequences: "))
+    sequences = int(input("Sequences: ")) + 1
 
     fibonacciSequence(firstSequence, secondSequence, sequences)
 
@@ -15,7 +15,7 @@ def fibonacciSequence(num1, num2, num3=10):
     for fibonacci in range(num3):
         sequence.append(sequence[-1] + sequence[-2])
 
-        for _ in range(fibonacci): 
+        for _ in range(2, fibonacci): 
             ratio.append(sequence[-1] / sequence[-2])
 
     goldenRatio = mean(ratio)
