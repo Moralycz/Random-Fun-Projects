e8534accb429dccc713435b2d957d99559c96d35
Moralycz <144354307+Moralycz@users.noreply.github.com>
2023-10-21 12:21:59 +0800

    Create fibonacci calc.py

---
+++ fibonacci calc.py
@@ -0,0 +1,26 @@
+from statistics import mean
+
+def main():
+    firstSequence = int(input("First Sequence: "))
+    secondSequence = int(input("Second Sequence: "))
+    sequences = int(input("Sequences: "))
+
+    fibonacciSequence(firstSequence, secondSequence, sequences)
+
+
+def fibonacciSequence(num1, num2, num3=10):
+    sequence = [num1, num2]
+    ratio = []
+
+    for fibonacci in range(num3):
+        sequence.append(sequence[-1] + sequence[-2])
+
+        for _ in range(fibonacci): 
+            ratio.append(sequence[-1] / sequence[-2])
+
+    goldenRatio = mean(ratio)
+
+    print(f"Fibonacci Sequence: {str(sequence)[1:-1]}")
+    print(f"Golden Ratio: {goldenRatio:.5f}")
+
+main()
